15739ddb682542d604e5d54a637f4f068c5c6e5e
Manu Gonzalez <manuelsoon@gmail.com>
2025-05-05 09:56:14 +0200

    fix props

--- app/[locale]/unit/[name]/page.tsx
+++ app/[locale]/unit/[name]/page.tsx
@@ -3,11 +3,12 @@ import { getUnitName } from "@/lib/units/get-unit-name";
 import UnitPageClient from "./unit-page-client";
 import { getUnitNameLine } from "@/lib/units/get-units-from-line";
 
-export async function generateMetadata({
-  params,
-}: {
+type Props = {
   params: { name: string; locale: string };
-}): Promise<Metadata> {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { name, locale } = await Promise.resolve(params);
   const unitName = getUnitName(name, locale);
 
