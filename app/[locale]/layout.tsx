839e039ee608b8441c3ef8e550dd29b6317ca166
Manu Gonzalez <manuelsoon@gmail.com>
2025-05-05 10:08:33 +0200

    add metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -14,6 +14,48 @@ export const metadata: Metadata = {
   icons: {
     icon: "/icon.png",
   },
+  keywords: [
+    "Age of Empires II",
+    "Counter Units",
+    "Units",
+    "Units Counter",
+    "aoe units",
+    "aoe2",
+    "aoe2 units",
+    "aoe2 counters",
+    "aoe2 counter units",
+    "aoe2 counter",
+    "aoe2 counter units",
+    "units",
+    "units counter",
+    "units counter units",
+    "units counter",
+    "units counter units",
+    "units counter",
+    "units counter units",
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      nocache: true,
+    },
+  },
+  authors: [{ name: "AoeUnits", url: "https://aoeunits.com" }],
+  generator: "AoeUnits",
+  applicationName: "AoeUnits",
+  category: "Games",
+  creator: "AoeUnits",
+  publisher: "AoeUnits",
+  openGraph: {
+    title: "AoeUnits",
+    description: "Search and discover Age of Empires II counter units",
+    url: "https://aoeunits.com",
+    siteName: "AoeUnits",
+  },
 };
 
 export default function RootLayout({
