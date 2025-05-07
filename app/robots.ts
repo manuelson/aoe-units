fd3d86b0881023bb631e51c874c2c7e7f9c2c1c7
Manu Gonzalez <manuelsoon@gmail.com>
2025-05-07 15:51:03 +0200

    Sitemap https

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "http://aoeunits.com/sitemap.xml",
+    sitemap: "https://aoeunits.com/sitemap.xml",
   };
 }
