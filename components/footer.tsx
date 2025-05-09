a487e712c2cc656855244104bb20b2f8ebc65001
Manu Gonzalez <manuelsoon@gmail.com>
2025-05-09 13:11:25 +0200

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,23 @@ import { useTranslations } from "next-intl";
 export const Footer = () => {
   const t = useTranslations();
   return (
-    <footer className="w-full py-6 text-center text-sm text-muted-foreground border-t border-border/40">
+    <footer className="w-full py-6 text-center text-sm text-muted-foreground border-t border-border/40 ">
       <div className="text-center">
-        <div>
-          {t("footer", { year: new Date().getFullYear() })} - Age of empires II
-          counter units.
+        <div className="text-xs">
+          <p>
+            {t("footer", { year: new Date().getFullYear() })} - Age of empires
+            II counter units.
+          </p>
+          <p className="text-gray-400">
+            The website was created under Microsoft's{" "}
+            <a
+              className="underline"
+              href="https://www.xbox.com/en-us/developers/rules"
+            >
+              Game Content Usage Rules
+            </a>{" "}
+            using assets from Age of Empires II.
+          </p>
         </div>
       </div>
     </footer>
